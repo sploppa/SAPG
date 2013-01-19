dbe2a3716800f736d8a17f15c746a3551d640ac2
Sebastian <sploppa@googlemail.com>
2013-01-19 13:58:48 +0100

    Android project converted to PhoneGap project

--- project-android/src/de/dhbwStuttgart/anelPwrCtrl/LoadPhoneGapActivity.java
+++ project-android/src/de/dhbwStuttgart/anelPwrCtrl/LoadPhoneGapActivity.java
@@ -1,22 +1,13 @@
 package de.dhbwStuttgart.anelPwrCtrl;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import org.apache.cordova.*;
 
-public class LoadPhoneGapActivity extends Activity {
+public class LoadPhoneGapActivity extends DroidGap {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_load_phone_gap);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_load_phone_gap, menu);
-        return true;
-    }
-    
+        super.loadUrl("file:///android_asset/www/index.html");
+    }    
 }
