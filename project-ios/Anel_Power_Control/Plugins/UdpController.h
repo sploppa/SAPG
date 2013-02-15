40dbff1f2a23fe3cdc31849c3062c2ea06cf268b
Sploppa <sploppa@googlemail.com>
2013-02-15 13:11:19 +0100

    wrong path

---
+++ project-ios/Anel_Power_Control/Plugins/UdpController.h
@@ -0,0 +1,13 @@
+//
+//  UdpController.h
+//  Anel_Power_Control
+//
+//  Created by Baskus Marti on 15.02.13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UdpController : NSObject
+
+@end
---
+++ project-ios/Anel_Power_Control/Plugins/UdpController.m
@@ -0,0 +1,13 @@
+//
+//  UdpController.m
+//  Anel_Power_Control
+//
+//  Created by Baskus Marti on 15.02.13.
+//
+//
+
+#import "UdpController.h"
+
+@implementation UdpController
+
+@end
