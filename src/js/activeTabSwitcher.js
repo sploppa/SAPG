c4fe4ac3082339609e56491e24312cf1bcf17ee6
Sebastian <sploppa@googlemail.com>
2013-04-11 20:00:44 +0200

    design overwork

--- src/js/activeTabSwitcher.js
+++ src/js/activeTabSwitcher.js
@@ -2,78 +2,27 @@
 * Author: Marco Kuiper (http://www.marcofolio.net/)
 */
 google.load("jquery", "1.3.1");
-google.setOnLoadCallback(function(){
-	$("#menu_tabs table tr td").click(function(e) {
-		e.preventDefault();
-		
-		$("#menu_tabs table tr td").each(function() {
-			$(this).removeClass("active");	
-		});
-		
-		$(this).addClass("active");
-		
-		$("h3").html($(this).attr("title"));
-		
-			if($("#ctrl").hasClass("active")){
-				$("#steuerung").show();
-				$("#doseSetting").show();
-		   		$(".menu_dosen_table").show();
-			}else{
-				$("#steuerung").hide();
-				$("#doseSetting").hide();
-		 	 	$(".menu_dosen_table").hide();
-			}
-			if($("#menu").hasClass("show")){
-				$("#menu").removeClass("show");
-				$("#menu").addClass("hide");
-			}else{
-				$("#menu").removeClass("hide");
-				$("#menu").addClass("show");
-			}
-		
-	});
-	
-	$(".menu_dosen_table tr td").click(function(e){
-		e.preventDefault();
-		
-		$(".menu_dosen_table tr td").each(function() {
-			$(this).removeClass("active");	
-		});
-		
-		$(this).addClass("active");
-		
-		$("h3").html($(this).attr("title"));
-		
-			if($("#ctrl").hasClass("active")){
-				$("#steuerung").show();
-				$("#doseSetting").show();
-		   		$(".menu_dosen_table").show();
-			}else{
-				$("#steuerung").hide();
-				$("#doseSetting").hide();
-		 	 	$(".menu_dosen_table").hide();
-			}
-	});
-	
+google.setOnLoadCallback(function(){	
 	$(document).ready(function() {
 		// Set default to "#steuerung"
-		$(".actions div").hide();
-		$("#steuerung").show();
-		$("#doseSetting").show();
 
 		// change div on click
-	    $("td.tabs a").click(function() {
-	        $(".actions div").hide();
-	        $($(this).attr("href")).show();
-			if($("#ctrl").hasClass("active")){
-				$("#steuerung").show();
-				$("#doseSetting").show();
-		   		$(".menu_dosen_table").show();
-			}else{
-				$("#steuerung").hide();
-				$("#doseSetting").hide();
-		 	 	$(".menu_dosen_table").hide();
-			}
+	    $(".header .tabs").click(function() {
+	    	if($(this).hasClass("active")){
+	    		$(this).removeClass("active");
+	    		
+	    		//$("#menu").removeClass("hide");
+	    		//$("#menu").addClass("show");
+	    		$(".actions").slideUp();
+	    	}else{
+	    		$(".header .tabs").removeClass("active");
+	      		$(this).addClass("active");
+	      		
+	    		//$("#menu").removeClass("show");
+	    		//$("#menu").addClass("hide");
+	    		
+	    		$(".actions").slideDown();
+	      	}
 	    });
 	});
 });
