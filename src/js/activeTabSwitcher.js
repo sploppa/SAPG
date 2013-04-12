06134813a02d4d3e517779c4473052712eb84718
Sploppa <sploppa@googlemail.com>
2013-04-12 13:29:49 +0200

    max-height min-height menu iOS works

--- src/js/activeTabSwitcher.js
+++ src/js/activeTabSwitcher.js
@@ -5,21 +5,16 @@ google.load("jquery", "1.3.1");
 google.setOnLoadCallback(function(){	
 	$(document).ready(function() {
 		// Set default to "#steuerung"
-
+        $(".actions").hide();
 		// change div on click
 	    $(".header .tabs").click(function() {
 	    	if($(this).hasClass("active")){
 	    		$(this).removeClass("active");
 	    		
-	    		//$("#menu").removeClass("hide");
-	    		//$("#menu").addClass("show");
 	    		$(".actions").slideUp();
 	    	}else{
 	    		$(".header .tabs").removeClass("active");
 	      		$(this).addClass("active");
-	      		
-	    		//$("#menu").removeClass("show");
-	    		//$("#menu").addClass("hide");
 	    		
 	    		$(".actions").slideDown();
 	      	}
